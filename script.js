efeb09ad99caa6c39af5b54440a8fba1f3390b8d
Daniastud <d.astudillod@uniandes.edu.co>
2026-03-02 16:46:46 +0000

    cambios nuevos en java

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("hola");
+
+const titulo = document.getElementById("titulo")
+
+titulo.addEventListener("click", () => {
+    titulo.innerText = "¡Buenos dìas!";
+    titulo.style.backgroundColor = "black";
+})
